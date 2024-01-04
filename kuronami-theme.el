4d0a9e5f789e5768a0c2ea7dec31f98ea95c7372
inj0h <14225024+inj0h@users.noreply.github.com>
2024-01-04 09:41:54 -0600

    Add Release 2.0
    Updates listed below
    
    - Theme:
      - Update prose
      - Update colors:
    
        | Old Name  | New Name | Old Color | New Color |
        |-----------|----------|-----------|-----------|
        | black-00  | black0   | #181a26   | unchanged |
        | black-01  | black1   | #202129   | #232328   |
        | black-02  | black2   | #2f303b   | #464751   |
        | blue-00   | blue0    | #2e41ac   | unchanged |
        | blue-01   | blue1    | #7fbbe9   | unchanged |
        | blue-02   | blue2    | #abbfda   | #a5bad7   |
        | gray-00   | gray0    | #595959   | #b3b3b3   |
        | gray-01   | gray1    | #b3b3b3   | #c9c9c9   |
        | green-00  | green0   | #708b4c   | unchanged |
        | green-01  | green1   | #668b8b   | unchanged |
        | green-02  | green2   | #65bab4   | unchanged |
        | green-03  | green3   | #bbe97f   | unchanged |
        | orange-00 | removed  | #e9ad7f   | removed   |
        | red-00    | red0     | #e97f86   | unchanged |
        | white-00  | white0   | #fffafa   | unchanged |
        | yellow-00 | yellow0  | #d1b897   | #cdb38f   |
    
      - Update Faces:
        - Update line-number to inherit from default, which allows scaling when
          calling text-scale-adjust
        - Simplify flyspell-incorrect Face expression
        - Add theme color for corfu-current
        - Updates: markdown-table, mode-line-inactive, whitespace-line
        - Removals: org, rust
    - Readme:
      - Update MELPA link - now points to the kuronami package page
      - Update prose
      - Update screencap to show theme color changes
    - Etc:
      - Simplify gitignore

--- kuronami-theme.el
+++ kuronami-theme.el
@@ -1,7 +1,7 @@
 ;;; kuronami-theme.el --- A deep blue theme with cool autumnal colors -*- lexical-binding: t; -*-
 
-;; Author: Eric Chung <>
-;; Version: 1.0.1
+;; Author: inj0h <>
+;; Version: 2.0
 ;; Filename: kuronami-theme.el
 ;; Package-Requires: ((emacs "24.1"))
 ;; URL: https://github.com/inj0h/kuronami
@@ -13,160 +13,142 @@
 ;; An Emacs theme with cool autumnal colors against a deep blue background.
 ;; Inspired by other Emacs themes - Deeper Blue shipped with most modern
 ;; versions of GNU Emacs, Gruber Darker by Alexey Kutepov, and Naysayer by
-;; Nick Aversano. The character designs of Rei Ayanami and
-;; Rei Ayanami (Tentative Name) from the anime Neon Genesis Evangelion by
+;; Nick Aversano. The character designs of Ayanami Rei and
+;; Ayanami Rei (Tentative Name) from the anime Neon Genesis Evangelion by
 ;; Studio Khara/Gainax provided the main source of inspiration regarding the
 ;; central ideas behind the colors used in this theme.
-
+;;
 ;; This theme advocates the oxymoronic minimalist Emacs user. Popular third
 ;; party packages most likely do not have overwritten colors. Further, many
 ;; default Faces that Inherit other default Faces do not get overwritten.
 ;; Generally, only Faces that do not use Inherit have themed coloring though
 ;; exceptions exist.
-
-;; A total of sixteen colors make up this theme. Eight of them stolen from other
-;; Emacs themes or sampled from the Internet using color identifying software.
-;; The other eight then derive from a subset of this first half. The website
+;;
+;; A total of fifteen colors make up this theme. Seven of them originate from
+;; other Emacs themes or various Internet samples using color identifying
+;; software. The remaining colors then derive from these seven. The website
 ;; https://www.color-hex.com provided much needed assistance in selecting these
 ;; derived colors according to the Analogous, Complementary, Triadic, Tint, and
 ;; Shade properties provided there. This theme's color declarations contain
 ;; in-line comments, attempting to explain the respective derivation process in
 ;; addition to crediting sources accordingly.
+;;
+;; Kuronami colors with the same name generally have a dark-less-intense to a
+;; light-more-intense arrangement such that the former gets a lower number and
+;; the latter, a higher number. For example, "kuronami-blue0" has a darker, less
+;; intense blue than "kuronami-blue1," which has a lighter, more intense blue.
+;;
+;; The author has historically struggled seeing and describing colors as most
+;; others do, so please have patience if these descriptions do not make sense.
 
 ;;; Code:
 
 (deftheme kuronami
-  "Kuronami theme for Emacs. Cool colors against a deep blue background.")
+  "Cool autumnal colors against a deep blue background.")
 
-;; NOTE: Kuronami colors with the same name generally have a dark-less-intense
-;;       to a light-more-intense arrangement such that the former gets a lower
-;;       number and the latter, a higher number. For example, "kuronami-blue-00"
-;;       has a darker, less intense blue than "kuronami-blue-01," which has a
-;;       lighter, more intense blue.
-;;
-;;       The author has historically struggled seeing and describing colors the
-;;       way others do so please show some leniency if these descriptions do not
-;;       make sense.
-;;
-;;       As mentioned earlier, each color declaration has an in-line comment
-;;       describing its origin or derivation.
-;;
-(let ((kuronami-black-00  "#181a26")  ; Stolen from the Deeper Blue Emacs theme.
-      (kuronami-black-01  "#202129")  ; black-02 -> 3 Shades darker.
-      (kuronami-black-02  "#2f303b")  ; black-00 -> 1 Tint lighter.
-      (kuronami-blue-00   "#2e41ac")  ; Sampled from an image of NGE Unit-00 (blue).
-      (kuronami-blue-01   "#7fbbe9")  ; Sampled from official Ayanami Blue!
-      (kuronami-blue-02   "#abbfda")  ; yellow-00 -> Complementary #97b0d1 -> 2 Tints lighter.
-      (kuronami-gray-00   "#595959")  ; Stolen from Emacs default "gray/grey 35."
-      (kuronami-gray-01   "#b3b3b3")  ; Stolen from Emacs default "gray/grey 70."
-      (kuronami-green-00  "#708b4c")  ; green-03 -> 4 Shades darker.
-      (kuronami-green-01  "#668b8b")  ; Stolen from Emacs default "pale turquoise 4."
-      (kuronami-green-02  "#65bab4")  ; blue-01 -> Analogous #7fe9e2 -> 2 Shades darker.
-      (kuronami-green-03  "#bbe97f")  ; blue-01 -> Triadic.
-      (kuronami-orange-00 "#e9ad7f")  ; blue-01 -> Complementary.
-      (kuronami-red-00    "#e97f86")  ; blue-01 -> Triadic #e97fbb -> Analogous.
-      (kuronami-white-00  "#fffafa")  ; Stolen from Emacs default "snow."
-      (kuronami-yellow-00 "#d1b897")) ; Stolen from the Naysayer Emacs theme.
+(let ((kuronami-black0  "#181a26")  ; Emacs default theme Deeper Blue
+      (kuronami-black1  "#232328")  ; black2 -> 5 shades darker
+      (kuronami-black2  "#464751")  ; black0 -> 2 tints lighter
+      (kuronami-blue0   "#2e41ac")  ; Random image of blue Unit-00
+      (kuronami-blue1   "#7fbbe9")  ; Official Ayanami Blue!
+      (kuronami-blue2   "#a5bad7")  ; yellow0 -> complementary #8fa9cd -> 2 tints lighter
+      (kuronami-gray0   "#b3b3b3")  ; Emacs default "gray/grey 70"
+      (kuronami-gray1   "#c9c9c9")  ; gray0 -> 3 tints lighter
+      (kuronami-green0  "#708b4c")  ; green3 -> 4 shades darker
+      (kuronami-green1  "#668b8b")  ; Emacs default "pale turquoise 4"
+      (kuronami-green2  "#65bab4")  ; blue1 -> analogous #7fe9e2 -> 2 shades darker
+      (kuronami-green3  "#bbe97f")  ; blue1 -> triadic
+      (kuronami-red0    "#e97f86")  ; blue1 -> triadic #e97fbb -> analogous
+      (kuronami-white0  "#fffafa")  ; Emacs default "snow"
+      (kuronami-yellow0 "#cdb38f")) ; Naysayer #d1b897 -> 1 tint lighter #d5bfa1 -> 1 mono darker
   (custom-theme-set-faces
    `kuronami
 
-   ;;; Vanilla Faces:
+   ;;; Vanilla:
 
-   ;; UI Elements:
-   `(cursor              ((t (:background ,kuronami-red-00))))
-   `(default             ((t (:background ,kuronami-black-00 :foreground ,kuronami-yellow-00))))
-   `(error               ((t (:foreground ,kuronami-red-00 :weight bold))))
-   `(fringe              ((t (:background ,kuronami-black-00))))
-   `(highlight           ((t (:background ,kuronami-green-00))))
-   `(isearch             ((t (:background ,kuronami-red-00 :foreground ,kuronami-black-00))))
-   `(lazy-highlight      ((t (:background ,kuronami-green-01))))
-   `(line-number         ((t (:foreground ,kuronami-blue-02))))
-   `(link                ((t (:foreground ,kuronami-green-02 :bold t :underline t))))
-   `(link-visited        ((t (:foreground ,kuronami-yellow-00 :bold t :underline t))))
+   ;; UI:
+   `(cursor              ((t (:background ,kuronami-red0))))
+   `(default             ((t (:background ,kuronami-black0 :foreground ,kuronami-gray1))))
+   `(error               ((t (:foreground ,kuronami-red0 :weight bold))))
+   `(fringe              ((t (:background ,kuronami-black0))))
+   `(highlight           ((t (:background ,kuronami-green0))))
+   `(isearch             ((t (:background ,kuronami-red0 :foreground ,kuronami-black0))))
+   `(lazy-highlight      ((t (:background ,kuronami-green1))))
+   `(line-number         ((t (:inherit default :foreground ,kuronami-blue2))))
+   `(link                ((t (:foreground ,kuronami-green2 :bold t :underline t))))
+   `(link-visited        ((t (:foreground ,kuronami-gray1 :bold t :underline t))))
    `(match               ((t (:inherit lazy-highlight))))
-   `(minibuffer-prompt   ((t (:foreground ,kuronami-white-00))))
-   `(region              ((t (:extend nil :background ,kuronami-blue-00)))) ; Mark to one past the final non-whitespace character kinda like Vi!
-   `(show-paren-match    ((t (:background ,kuronami-blue-02))))
-   `(show-paren-mismatch ((t (:background ,kuronami-red-00))))
-   `(success             ((t (:foreground ,kuronami-green-03 :weight bold))))
-   `(warning             ((t (:foreground ,kuronami-orange-00 :weight bold))))
+   `(minibuffer-prompt   ((t (:foreground ,kuronami-white0))))
+   `(region              ((t (:extend nil :background ,kuronami-blue0)))) ; Like Vim!
+   `(show-paren-match    ((t (:background ,kuronami-blue2))))
+   `(show-paren-mismatch ((t (:background ,kuronami-red0))))
+   `(success             ((t (:foreground ,kuronami-green3 :weight bold))))
+   `(warning             ((t (:foreground ,kuronami-yellow0 :weight bold))))
 
    ;; Font Lock:
-   `(font-lock-builtin-face           ((t (:foreground ,kuronami-white-00))))
-   `(font-lock-comment-face           ((t (:foreground ,kuronami-blue-01 :italic t))))
+   `(font-lock-builtin-face           ((t (:foreground ,kuronami-blue2))))
+   `(font-lock-comment-face           ((t (:foreground ,kuronami-blue1 :italic t))))
    `(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
-   `(font-lock-constant-face          ((t (:foreground ,kuronami-orange-00))))
-   `(font-lock-doc-face               ((t (:foreground ,kuronami-green-00))))
-   `(font-lock-function-name-face     ((t (:foreground ,kuronami-blue-02))))
+   `(font-lock-constant-face          ((t (:foreground ,kuronami-yellow0))))
+   `(font-lock-doc-face               ((t (:foreground ,kuronami-green0))))
+   `(font-lock-function-name-face     ((t (:foreground ,kuronami-white0))))
    `(font-lock-keyword-face           ((t (:inherit font-lock-builtin-face))))
-   `(font-lock-negation-char-face     ((t (:foreground ,kuronami-red-00))))
-   `(font-lock-preprocessor-face      ((t (:foreground ,kuronami-green-03))))
-   `(font-lock-string-face            ((t (:foreground ,kuronami-green-02))))
+   `(font-lock-negation-char-face     ((t (:foreground ,kuronami-red0))))
+   `(font-lock-preprocessor-face      ((t (:foreground ,kuronami-green3))))
+   `(font-lock-string-face            ((t (:foreground ,kuronami-green2))))
    `(font-lock-type-face              ((t (:inherit font-lock-constant-face))))
    `(font-lock-variable-name-face     ((t (:inherit font-lock-function-name-face))))
-   `(font-lock-warning-face           ((t (:foreground ,kuronami-red-00 :bold t :italic t))))
+   `(font-lock-warning-face           ((t (:foreground ,kuronami-red0 :bold t :italic t))))
 
-   ;;; From this point, the Faces get organized alphabetically.
+   ;; Alphabetically from this point.
 
    ;; Compilation:
-   `(compilation-column-number  ((t (:foreground ,kuronami-white-00))))
-   `(compilation-line-number    ((t (:foreground ,kuronami-blue-02))))
+   `(compilation-column-number  ((t (:foreground ,kuronami-white0))))
+   `(compilation-line-number    ((t (:foreground ,kuronami-blue2))))
    `(compilation-mode-line-exit ((t (:inherit compilation-info))))
    `(compilation-mode-line-fail ((t (:inherit compilation-error))))
 
    ;; Completions:
-   `(completions-common-part      ((t (:foreground ,kuronami-white-00))))
-   `(completions-first-difference ((t (:foreground ,kuronami-orange-00 :bold t))))
+   `(completions-common-part      ((t (:foreground ,kuronami-white0))))
+   `(completions-first-difference ((t (:foreground ,kuronami-yellow0 :bold t))))
 
    ;; Flyspell:
-   `(flyspell-duplicate ((t nil))) ; Setting "flyspell-duplicate-distance" does not work for Emacs 27.2 on macOS x86 so disable the Face. For now.
-   `(flyspell-incorrect
-     ((((supports :underline (:style wave))) (:underline (:color ,kuronami-red-00 :style wave)))
-      (t (:foreground ,kuronami-red-00 :underline t :weight bold)))) ; For terminal Emacsclient.
+   `(flyspell-duplicate ((t nil))) ; Setting "flyspell-duplicate-distance" does not work for Emacs 27.2 on macOS x86 so disable the Face.
+   `(flyspell-incorrect ((t (:underline (:color ,kuronami-red0 :style wave)))))
 
    ;; Ido:
-   `(ido-first-match ((t (:foreground ,kuronami-orange-00 :italic t))))
-   `(ido-only-match  ((t (:foreground ,kuronami-green-03 :bold t :italic t))))
-   `(ido-subdir      ((t (:foreground ,kuronami-blue-02))))
+   `(ido-first-match ((t (:foreground ,kuronami-yellow0 :italic t))))
+   `(ido-only-match  ((t (:foreground ,kuronami-green3 :bold t :italic t))))
+   `(ido-subdir      ((t (:foreground ,kuronami-white0))))
 
    ;; Mode Line:
-   `(mode-line           ((t (:background ,kuronami-gray-01 :foreground ,kuronami-black-00)))) ; Just colors. No "boxing" effect.
+   `(mode-line           ((t (:background ,kuronami-gray0 :foreground ,kuronami-black1)))) ; Just colors. No "boxing" effect.
    `(mode-line-buffer-id ((t nil)))
    `(mode-line-emphasis  ((t nil)))
-   `(mode-line-inactive  ((t (:background ,kuronami-gray-00 :foreground ,kuronami-gray-01))))
-
-   ;; Org:
-   `(org-block ((t (:extend t :background ,kuronami-black-01 :foreground ,kuronami-gray-01))))
-   `(org-done  ((t (:foreground ,kuronami-green-03 :weight bold))))
-   `(org-table ((t (:foreground ,kuronami-blue-01))))
-   `(org-todo  ((t (:foreground ,kuronami-orange-00 :weight bold))))
+   `(mode-line-inactive  ((t (:background ,kuronami-black2 :foreground ,kuronami-gray0))))
 
    ;; Whitespace:
    `(trailing-whitespace         ((t (:inherit whitespace-trailing)))) ; Different from whitespace-trailing? Shrug.
-   `(whitespace-empty            ((t (:background ,kuronami-red-00 :foreground ,kuronami-black-02))))
-   `(whitespace-hspace           ((t (:background ,kuronami-black-00 :foreground ,kuronami-black-02))))
+   `(whitespace-empty            ((t (:background ,kuronami-red0 :foreground ,kuronami-black2))))
+   `(whitespace-hspace           ((t (:background ,kuronami-black0 :foreground ,kuronami-black2))))
    `(whitespace-indentation      ((t (:inherit whitespace-empty))))
-   `(whitespace-line             ((t (:background ,kuronami-black-00 :foreground ,kuronami-red-00))))
-   `(whitespace-newline          ((t (:background ,kuronami-black-00 :foreground ,kuronami-black-02))))
-   `(whitespace-space            ((t (:background ,kuronami-black-00 :foreground ,kuronami-black-02))))
+   `(whitespace-line             ((t (:background ,kuronami-black0 :foreground ,kuronami-black2))))
+   `(whitespace-newline          ((t (:background ,kuronami-black0 :foreground ,kuronami-black2))))
+   `(whitespace-space            ((t (:background ,kuronami-black0 :foreground ,kuronami-black2))))
    `(whitespace-space-after-tab  ((t (:inherit whitespace-empty))))
    `(whitespace-space-before-tab ((t (:inherit whitespace-empty))))
-   `(whitespace-tab              ((t (:background ,kuronami-black-00 :foreground ,kuronami-black-02))))
+   `(whitespace-tab              ((t (:background ,kuronami-black0 :foreground ,kuronami-black2))))
    `(whitespace-trailing         ((t (:inherit whitespace-empty))))
 
-   ;;; Programming Language Faces:
-   ;; Nothing here yet.
+   ;;; Third Party:
 
-   ;;; Third Party Faces:
-
-   ;;; Programming Language Faces:
+   ;; Corfu:
+   `(corfu-current ((t (:background ,kuronami-green1))))
 
    ;; Markdown:
-   `(markdown-code-face             ((t (:extend t :background ,kuronami-black-01 :foreground ,kuronami-gray-01))))
-   `(markdown-language-keyword-face ((t (:foreground ,kuronami-blue-01))))
-
-   ;; Rust:
-   `(rust-builtin-formatting-macro ((t (:inherit font-lock-preprocessor-face)))))) ; Macros get one color.
+   `(markdown-code-face             ((t (:extend t :background ,kuronami-black1 :foreground ,kuronami-gray0))))
+   `(markdown-language-keyword-face ((t (:foreground ,kuronami-blue1))))
+   `(markdown-table-face            ((t (:foreground ,kuronami-gray0))))))
 
 ;;;###autoload
 (when (and (boundp 'custom-theme-load-path) load-file-name)
--- readme.md
+++ readme.md
@@ -1,10 +1,9 @@
 [![MELPA](https://melpa.org/packages/kuronami-theme-badge.svg)](https://melpa.org/#/kuronami-theme)
-# Kuronami (&#40658;&#27874;) Theme
-An Emacs theme with cool autumnal colors against a deep blue background. &#127809; &#10052; &#127754;
+# Kuronami (&#40658;&#27874;)
+An Emacs theme with cool autumnal colors against a deep blue background. &#127809; &#10052; &#127754; <!-- emoji = autumn leaf, snowflake, wave -->
 
 Inspired by Deeper Blue (GNU Emacs default), [Gruber Darker,](https://github.com/rexim/gruber-darker-theme) and [Naysayer.](https://github.com/nickav/naysayer-theme.el)
 
-# Preview
 ![screencap](screencap.png)
 
 # Installation
@@ -12,22 +11,20 @@ Inspired by Deeper Blue (GNU Emacs default), [Gruber Darker,](https://github.com
 You must have Emacs version 24.0 or later to use this theme as intended.
 
 ## Package Manager
-Kuronami is readily available on [MELPA.](https://melpa.org) You can obtain a copy by configuring package.el accordingly or via some alternative like [straight](https://github.com/raxod502/straight.el) or [Nix.](https://nixos.org/)
+Kuronami is readily available on [MELPA.](https://melpa.org/#/kuronami-theme) You can obtain a copy by configuring package.el or some alternative accordingly.
 
-``` emacs-lisp
+```emacs-lisp
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ```
 
-## Manual
+## From Source
 Download a copy of kuronami-theme.el by cloning this repository or copying the file as plain text. Then tell Emacs where to look for it on your system.
 
-``` emacs-lisp
-(add-to-list 'custom-theme-load-path "~/path/to/directory/with/downloaded/copy/")
+```emacs-lisp
+(add-to-list 'custom-theme-load-path "~/path/to/directory/with/downloaded/copy")
 (load-theme 'kuronami t)
 ```
 
 # Commentary
-This theme advocates the oxymoronic minimalist Emacs user. Popular third party packages most likely do not have overwritten colors. Further, many default Faces that Inherit other default Faces do not get overwritten. Generally, only Faces that do not use Inherit have themed coloring though exceptions exist.
-
-In all likelihood, fork at your own convenience.
+This theme advocates the oxymoronic minimalist Emacs user and aims to keep a small scope. Many popular packages bundled with Emacs (Org and Magit) or provided by third parties do not have and will most likely continue to not have themed colors.
